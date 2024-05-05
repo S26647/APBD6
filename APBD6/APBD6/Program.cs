71fc252d85b16b6eceb127496195c4ac68a73200
Kacper-Szczygiel <kacpi.szczygiel@gmail.com>
2024-05-05 18:39:34 +0200

    finished

--- APBD6/APBD6/Program.cs
+++ APBD6/APBD6/Program.cs
@@ -1,23 +1,26 @@
 using APBD6.Repos;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddScoped<IAnimalRepo, AnimalRepo>();
-//builder.Services.AddScoped<IAnimalService, AnimalService>();AddEndpointsApiExplorer
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<IAnimalRepo, AnimalRepo>();
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.MapControllers();
-
-app.Run();
